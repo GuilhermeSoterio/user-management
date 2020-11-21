ba46ba7d3967cdf09a5c363ebd072e365637fafa
Guilherme Sotério <guilhermehenrique.oliv3@gmail.com>
2020-11-21 16:54:01 -0300

    Criando uma variavel json, e aderindo suas propriedades dinamicamente. Ou seja, seu user.name está recebendo seu valor (value)

--- scripts/index.js
+++ scripts/index.js
@@ -1,24 +1,22 @@
-var name = document.querySelector("#exampleInputName");
-var gender = document.querySelectorAll("#example-user-create [name=gender]:checked");
-var birth = document.querySelector("#exampleInputBirth");
-var country = document.querySelector("#exampleInputCountry");
-var email = document.querySelector("#exampleInputEmail");
-var password = document.querySelector("#exampleInputPassword");
-var photo = document.querySelector("#exampleInputFile");
-var admin = document.querySelector("#exampleInputAdmin");
-
-
+//var name = document.querySelector("#exampleInputName");
+//var gender = document.querySelectorAll("#example-user-create [name=gender]:checked");
+//var birth = document.querySelector("#exampleInputBirth");
+//var country = document.querySelector("#exampleInputCountry");
+//var email = document.querySelector("#exampleInputEmail");
+//var password = document.querySelector("#exampleInputPassword");
+//var photo = document.querySelector("#exampleInputFile");
+//var admin = document.querySelector("#exampleInputAdmin");
 var fields = document.querySelectorAll("#form-user-create [name]");
-
+var user = {}
 fields.forEach(function(field, index){
 
     if (field.name == "gender") {
 
       if (field.checked) {
-        console.log("SIM",field);
+        user[field.name] = field.value;
       }
 
     } else {
-        console.log("NÃO");
+      user[field.name] = field.value;
     }
 });
