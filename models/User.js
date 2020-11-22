50dd5cf5acd28332ce84e79c27bcfd238862c9a8
Guilherme Sotério <guilhermehenrique.oliv3@gmail.com>
2020-11-22 18:06:44 -0300

    Implementing object-oriented paradigm, building model layer and user class

---
+++ models/User.js
@@ -0,0 +1,14 @@
+class user {
+
+    constructor(name, gender, birth, country, email, password, photo, admin){
+
+        this.name = name1;
+        this.gender = gender;
+        this.birth = birth;
+        this.country = country;
+        this.email = email;
+        this.password = password;
+        this.photo = photo;
+        this.admin = admin;
+    }
+}
--- scripts/index.js
+++ scripts/index.js
@@ -8,7 +8,34 @@
 //var admin = document.querySelector("#exampleInputAdmin");
 var fields = document.querySelectorAll("#form-user-create [name]");
 var user = {}
-fields.forEach(function(field, index){
+
+function addLine(dataUser){
+
+
+  document.getElementById("table-users").innerHTML = `
+          <tr>
+            <td>
+              <img src="dist/img/user1-128x128.jpg" alt="User Image" class="img-circle img-sm">
+            </td>
+            <td>${dataUser.name}</td>
+            <td>${dataUser.email}</td>
+            <td>${dataUser.admin}</td>
+            <td>${dataUser.birth}</td>
+            <td>
+              <button type="button" class="btn btn-primary btn-xs btn-flat">Editar</button>
+              <button type="button" class="btn btn-danger btn-xs btn-flat">Excluir</button>
+            </td>
+          </tr>
+  `;
+
+  document.getElementById("table-users").appendChild(tr);
+}
+
+document.getElementById("form-user-create").addEventListener("submit", function(event){
+
+  event.preventDefault();
+  
+  fields.forEach(function(field, index){
 
     if (field.name == "gender") {
 
@@ -19,4 +46,27 @@ fields.forEach(function(field, index){
     } else {
       user[field.name] = field.value;
     }
+
+  });
+//Criando um objeto da classe usuário, com a variavel objectUser
+  var objectUser = new User(
+    user.name,
+    user.gender,
+    user.birth,
+    user.country,
+    user.email, 
+    user.password, 
+    user.photo, 
+    user.admin
+  );
+
+  addLine(user);
+
+  
 });
+
+//document.querySelectorAll("Button").forEach(function(){
+//Dois parametros:nome do evento, e a função que quer que ocorra
+      //this.addEventListener("click", function(){
+      //console.log(Ok clicado)
+//});
