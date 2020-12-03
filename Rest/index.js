fd564a88d3841531d2ce388fb6afa8333a6f2c32
Guilherme Sotério <guilhermehenrique.oliv3@gmail.com>
2020-12-03 15:35:08 -0300

    Creating server with nodeJS and REST HTTP

---
+++ Rest/index.js
@@ -0,0 +1,38 @@
+const http = require ('http');
+
+let server = http.createServer((req, res)=>{
+    
+    console.log('URL:', req.url);
+    console.log('METHOD:', req.method);
+
+    switch (req.url) {
+        case '/':
+
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'text/html');
+        res.end('<h1>Olá</h1>');
+
+        break;
+
+        case '/users':
+        
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'application/json');
+        res.end(JSON.stringify({
+            users:[{
+                name: 'Hcode',
+                email:'contato@hcide.com.br',
+                id:1
+        }]
+    }));
+
+        break;
+    }
+});
+    server.listen(3000, '127.0.0.1', ()=>{
+
+        console.log('servidor rodando!');
+
+
+});
+
