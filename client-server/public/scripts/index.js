a25ba39353a7e5e1590ad0983464f1c34f998241
Guilherme Sotério <guilhermehenrique.oliv3@gmail.com>
2020-12-11 14:30:31 -0300

    Ajax, and nestify with POST and PUT methods

---
+++ client-server/public/scripts/index.js
@@ -0,0 +1,11 @@
+let userController = new UserController("form-user-create", "form-user-update", "table-users");
+
+
+//var name = document.querySelector("#exampleInputName");
+//var gender = document.querySelectorAll("#example-user-create [name=gender]:checked");
+//var birth = document.querySelector("#exampleInputBirth");
+//var country = document.querySelector("#exampleInputCountry");
+//var email = document.querySelector("#exampleInputEmail");
+//var password = document.querySelector("#exampleInputPassword");
+//var photo = document.querySelector("#exampleInputFile");
+//var admin = document.querySelector("#exampleInputAdmin");
